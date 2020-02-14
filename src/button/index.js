31fb75dd7a12f75268912d2521122c61ee549a6b
Christopher Dimoulis <christopherjamesd@gmail.com>
2020-02-14 14:39:42 -0600

    cleanup and add starting files

--- src/button/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Button = () => (
-    <button type='button'>CLICK ME</button>
-);
-
-export default Button;
--- stories/button.stories.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Button from '../src/button';
-
-export default {
-  title: 'Custom',
-  component: Button,
-};
-
-export const Basic = () => <Button />;
